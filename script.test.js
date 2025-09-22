ddf09d407dae6e97b6355ea686d0fd809d7836cd
c8in4 <106409156+c8in4@users.noreply.github.com>
2025-09-23 07:21:24 +1200

    Add tests

--- script.test.js
+++ script.test.js
@@ -1,9 +1,59 @@
-import { test } from "./script";
+import {} from "./script";
 
-describe("[scripttest] Test case", () => {
-  beforeEach(() => {});
+describe.skip("String manipulation", () => {
+  it("Capitalize 'test'", () => {
+    expect(capitalize("test")).toBe("Test");
+  });
+  it("Capitalize 'string'", () => {
+    expect(capitalize("string")).toBe("String");
+  });
 
-  it("Should ", () => {
-    expect(test()).toBe(3);
+  it("Reverse 'test'", () => {
+    expect(reverse("test")).toBe("tset");
+  });
+  it("Reverse 'string'", () => {
+    expect(reverse("string")).toBe("gnirts");
+  });
+});
+
+describe.skip("Calculator", () => {
+  it("add 2 and 3 to be 5", () => {
+    expect(add(2, 3)).toBe(5);
+  });
+  it("subtract 3 and 2 to be 1", () => {
+    expect(subtract(3, 2)).toBe(1);
+  });
+  it("multiply 2 and 3 to be 6", () => {
+    expect(multiply(2, 3)).toBe(6);
+  });
+  it("divide 6 and 3 to be 2", () => {
+    expect(divide(6, 3)).toBe(2);
+  });
+});
+
+describe.skip("Caesar cipher", () => {
+  it("'xyz' 3", () => {
+    expect(caesarCipher("xyz", 3)).toBe("abc");
+  });
+
+  it("'HeLLo' 3", () => {
+    expect(caesarCipher("HeLLo", 3)).toBe("KhOOr");
+  });
+
+  it("'Hello World!' 3", () => {
+    expect(1).toBe(1);
+  });
+});
+
+describe.skip("Analyze Array", () => {
+  const object = [[1, 8, 3, 4, 2, 6]];
+  const result = {
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  };
+  it("[1,8,3,4,2,6]", () => {
+    expect(analyzeArray(object)).toBe(result);
   });
 });
