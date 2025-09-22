8e677bccf366d39b42121960fb2658ea50778c18
c8in4 <106409156+c8in4@users.noreply.github.com>
2025-09-23 10:39:45 +1200

    Refactor 'it' to 'test'

--- script.test.js
+++ script.test.js
@@ -7,50 +7,50 @@ import {
 } from "./script";
 
 describe("String manipulation", () => {
-  it("Capitalize 'test'", () => {
+  test("Capitalize 'test'", () => {
     expect(capitalize("test")).toBe("Test");
   });
-  it("Capitalize 'string'", () => {
+  test("Capitalize 'string'", () => {
     expect(capitalize("string")).toBe("String");
   });
 
-  it("Reverse 'test'", () => {
+  test("Reverse 'test'", () => {
     expect(reverse("test")).toBe("tset");
   });
-  it("Reverse 'string'", () => {
+  test("Reverse 'string'", () => {
     expect(reverse("string")).toBe("gnirts");
   });
 });
 
 describe("Calculator", () => {
-  it("add 2 and 3 to be 5", () => {
+  test("add 2 and 3 to be 5", () => {
     expect(calculator.add(2, 3)).toBe(5);
   });
-  it("subtract 3 and 2 to be 1", () => {
+  test("subtract 3 and 2 to be 1", () => {
     expect(calculator.subtract(3, 2)).toBe(1);
   });
-  it("multiply 2 and 3 to be 6", () => {
+  test("multiply 2 and 3 to be 6", () => {
     expect(calculator.multiply(2, 3)).toBe(6);
   });
-  it("divide 6 and 3 to be 2", () => {
+  test("divide 6 and 3 to be 2", () => {
     expect(calculator.divide(6, 3)).toBe(2);
   });
 });
 
 describe("Caesar cipher", () => {
-  it("'xyz' 3", () => {
+  test("'xyz' 3", () => {
     expect(caesarCipher("xyz", 3)).toBe("abc");
   });
 
-  it("'HeLLo' 3", () => {
+  test("'HeLLo' 3", () => {
     expect(caesarCipher("HeLLo", 3)).toBe("KhOOr");
   });
 
-  it("'Hello World!' 3", () => {
+  test("'Hello World!' 3", () => {
     expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
   });
 
-  it("Big key", () => {
+  test("Big key", () => {
     expect(caesarCipher("abcABC xyzXYZ", 45)).toBe("tuvTUV qrsQRS");
   });
 });
@@ -63,7 +63,7 @@ describe("Analyze Array", () => {
     max: 8,
     length: 6,
   };
-  it("[1,8,3,4,2,6]", () => {
+  test("[1,8,3,4,2,6]", () => {
     expect(analyzeArray(object)).toEqual(result);
   });
 });
